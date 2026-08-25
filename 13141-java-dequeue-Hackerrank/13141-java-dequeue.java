4a71d24a5096dbb40dce512411db4aef94b42185
venu183 <vtu27596@veltech.edu.in>
2026-08-25 10:02:09 +0530

    Force committed solution

---
+++ 13141-java-dequeue-Hackerrank/13141-java-dequeue.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int unique = 0;
+        int maxUnique = 0;
+
+        // First window
+        for (int i = 0; i < M; i++) {
+            int value = arr[i];
+
+            if (!map.containsKey(value)) {
+                map.put(value, 1);
+                unique++;
+            } else {
+                map.put(value, map.get(value) + 1);
+            }
+        }
+
+        maxUnique = unique;
+
+        // Slide the window
+        for (int i = M; i < N; i++) {
+
+            // Remove the element going out
+            int outgoing = arr[i - M];
+
+            int count = map.get(outgoing);
+
+            if (count == 1) {
+                map.remove(outgoing);
+                unique--;
+            } else {
+                map.put(outgoing, count - 1);
+            }
+
+            // Add the new element
+            int incoming = arr[i];
+
+            if (!map.containsKey(incoming)) {
+                map.put(incoming, 1);
+                unique++;
+            } else {
+                map.put(incoming, map.get(incoming) + 1);
+            }
+
+            maxUnique = Math.max(maxUnique, unique);
+        }
+
+        System.out.println(maxUnique);
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
