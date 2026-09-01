fa431048fe44c0ed3a5aa5a805749bf29050565e
venu183 <vtu27596@veltech.edu.in>
2026-09-01 14:27:55 +0530

    ✅ Linked List Cycle II | Medium | Java | Runtime: 0 ms (100.00%) | Memory: 46.4 MB | 2026-09-01

---
+++ leetcode/medium/0142-linked-list-cycle-ii/solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        // Step 1: Detect whether a cycle exists
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                // Step 2: Find the beginning of the cycle
+                slow = head;
+
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+
+                return slow;
+            }
+        }
+
+        return null;
+    }
+}
