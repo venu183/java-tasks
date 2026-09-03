289b66354a357d96259216951d6222844058d7da
venu183 <vtu27596@veltech.edu.in>
2026-09-03 11:15:54 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 21400-compare-the-triplets-Hackerrank/21400-compare-the-triplets.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        int alice = 0;
+        int bob = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (a.get(i) > b.get(i)) {
+                alice++;
+            } else if (a.get(i) < b.get(i)) {
+                bob++;
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        result.add(alice);
+        result.add(bob);
+
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] aInput = br.readLine().split(" ");
+        String[] bInput = br.readLine().split(" ");
+
+        List<Integer> a = new ArrayList<>();
+        List<Integer> b = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            a.add(Integer.parseInt(aInput[i]));
+            b.add(Integer.parseInt(bInput[i]));
+        }
+
+        List<Integer> result = compareTriplets(a, b);
+
+        System.out.println(result.get(0) + " " + result.get(1));
+    }
+}
+
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
