053ce12a0302e1a34f253fd836bd294809183e7c
venu183 <vtu27596@veltech.edu.in>
2026-09-03 11:57:33 +0530

    Time: 254 ms (33.61%), Space: 117.4 MB (76.87%) - LeetHub

---
+++ 1600-throne-inheritance/1600-throne-inheritance.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class ThroneInheritance {
+
+    private String king;
+    private Map<String, List<String>> children;
+    private Set<String> dead;
+
+    public ThroneInheritance(String kingName) {
+        king = kingName;
+        children = new HashMap<>();
+        dead = new HashSet<>();
+    }
+
+    public void birth(String parentName, String childName) {
+        children.computeIfAbsent(parentName, k -> new ArrayList<>())
+                .add(childName);
+    }
+
+    public void death(String name) {
+        dead.add(name);
+    }
+
+    public List<String> getInheritanceOrder() {
+        List<String> result = new ArrayList<>();
+        dfs(king, result);
+        return result;
+    }
+
+    private void dfs(String person, List<String> result) {
+
+        // Add person if alive
+        if (!dead.contains(person)) {
+            result.add(person);
+        }
+
+        // Visit children in birth order
+        if (children.containsKey(person)) {
+            for (String child : children.get(person)) {
+                dfs(child, result);
+            }
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
